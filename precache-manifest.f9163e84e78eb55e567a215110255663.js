08364ef435be9550940ef5bcbf108ff62aa313f5
Michael Silva <msilva@aggienetwork.com>
2020-08-25 15:38:42 -0500

    Updates

---
+++ precache-manifest.f9163e84e78eb55e567a215110255663.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1313e73def8faa3995cd01d23696b59",
+    "url": "/silvafolio-2/index.html"
+  },
+  {
+    "revision": "637188bfee98c2cd660b",
+    "url": "/silvafolio-2/static/css/main.61713271.chunk.css"
+  },
+  {
+    "revision": "520445dfde9d5aac0044",
+    "url": "/silvafolio-2/static/js/2.ff1a21ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/silvafolio-2/static/js/2.ff1a21ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637188bfee98c2cd660b",
+    "url": "/silvafolio-2/static/js/main.3ce4db4d.chunk.js"
+  },
+  {
+    "revision": "b4726d16b83e18390aec",
+    "url": "/silvafolio-2/static/js/runtime-main.387ca50f.js"
+  },
+  {
+    "revision": "2d98ce29166bfb529e0cd1077c2abb10",
+    "url": "/silvafolio-2/static/media/0.2d98ce29.png"
+  },
+  {
+    "revision": "7d185630c263fc70153d252d716ec518",
+    "url": "/silvafolio-2/static/media/1.7d185630.png"
+  },
+  {
+    "revision": "aeec1ee484e6e2c87275fe63965f2e11",
+    "url": "/silvafolio-2/static/media/2.aeec1ee4.png"
+  },
+  {
+    "revision": "41d5a2921e3dd3a465b739b8c117962e",
+    "url": "/silvafolio-2/static/media/michael.41d5a292.png"
+  },
+  {
+    "revision": "cc1868a069afac6d367b005b8e3889be",
+    "url": "/silvafolio-2/static/media/ocean.cc1868a0.jpg"
+  }
+]);
